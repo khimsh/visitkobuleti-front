38698cf5df34c39f04a72590e0422805d18580c4
khimsh <i.khimsh@gmail.com>
2021-09-09 10:48:52 +0400

    Started on responsiveness.

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,30 @@ $(document).ready(function () {
     dots: true,
     slidesToShow: 4,
     slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 580,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   });
 
   $('.reservations').slick({
